27388bbbea482f9cb38d37fbc9ac1cf4f1cac11e
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-02-14 02:18:00 +0530

    Time: 5 ms (17.51%), Space: 45.1 MB (72.35%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,9 +1,10 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int []nge=new int[nums2.length];
+
         Stack<Integer> st=new Stack<>();
         for(int i=nums2.length-1;i>=0;i--){
-            while(st.size()!=0 &&st.peek()<=nums2[i]){
+            while(!st.isEmpty() &&nums2[i]>=st.peek()){
                 st.pop();
             }
             if(st.isEmpty()){
@@ -12,16 +13,18 @@ class Solution {
             else{
                 nge[i]=st.peek();
             }
-            st.push(nums2[i]);
-        }
-        int[] ans=new int[nums1.length];
-        for(int i=0;i<nums1.length;i++){
-            for(int j=0;j<nums2.length;j++)
+            st.push(nums2[i]); 
+        } 
+       int []ans=new int[nums1.length];
+
+       for(int i=0;i<nums1.length;i++){
+        for(int j=0;j<nums2.length;j++){
             if(nums1[i]==nums2[j]){
                 ans[i]=nge[j];
                 break;
             }
         }
-      return ans;  
+       }
+       return ans; 
     }
 }
