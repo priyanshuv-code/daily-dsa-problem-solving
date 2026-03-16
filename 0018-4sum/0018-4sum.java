9d7a63200a365a686a4cc82670a0690912981819
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-03-16 14:40:01 +0530

    Time: 174 ms (5.12%), Space: 47.7 MB (5.45%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,50 +1,31 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
 
-        List<List<Integer>> ans = new ArrayList<>();
-        int n = nums.length;
-        if (n < 4) return ans;
-
         Arrays.sort(nums);
+        Set<List<Integer>> set=new HashSet<>();
+        int n=nums.length;
+        for(int i=0;i<n-3;i++){
+            for(int j=i+1;j<n-2;j++){
+                int left=j+1;
+                int right=n-1;
 
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            for (int j = i + 1; j < n; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-
-                int k = j + 1;
-                int l = n - 1;
-
-                while (k < l) {
-                    long sum = 0;
-                    sum += nums[i];
-                    sum += nums[j];
-                    sum += nums[k];
-                    sum += nums[l];
-
-                    if (sum == target) {
-                        ans.add(Arrays.asList(
-                            nums[i], nums[j], nums[k], nums[l]
-                        ));
-
-                        k++;
-                        l--;
-
-                        while (k < l && nums[k] == nums[k - 1]) k++;
-                        while (k < l && nums[l] == nums[l + 1]) l--;
+                while(left<right){
+                    long sum=((long)nums[i]+nums[j]+nums[left]+nums[right]);
+                    if(sum==target){
+                        set.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
+                        left++;
+                        right--;;
                     }
-                    else if (sum < target) {
-                        k++;
+                    else if(sum<target){
+                        left++;
                     }
-                    else {
-                        l--;
+                    else{
+                        right--;
                     }
                 }
             }
         }
-        return ans;
+        
+        return new ArrayList<>(set);
     }
 }
