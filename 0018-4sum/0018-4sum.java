4133794216452fb919a06508bf3600a8e96122d1
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-03-16 14:36:57 +0530

    Time: 21 ms (42.75%), Space: 46 MB (29.69%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,48 +1,50 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
 
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
-        if (n < 4) return res;
+        if (n < 4) return ans;
 
         Arrays.sort(nums);
 
-        for (int i = 0; i < n - 3; i++) {
-            // skip duplicates for i
+        for (int i = 0; i < n; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            for (int j = i + 1; j < n - 2; j++) {
-                // skip duplicates for j
+            for (int j = i + 1; j < n; j++) {
                 if (j > i + 1 && nums[j] == nums[j - 1]) continue;
 
-                int left = j + 1;
-                int right = n - 1;
+                int k = j + 1;
+                int l = n - 1;
 
-                while (left < right) {
-                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                while (k < l) {
+                    long sum = 0;
+                    sum += nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
 
                     if (sum == target) {
-                        res.add(Arrays.asList(
-                            nums[i], nums[j], nums[left], nums[right]
+                        ans.add(Arrays.asList(
+                            nums[i], nums[j], nums[k], nums[l]
                         ));
 
-                        // skip duplicates for left & right
-                        while (left < right && nums[left] == nums[left + 1]) left++;
-                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        k++;
+                        l--;
 
-                        left++;
-                        right--;
+                        while (k < l && nums[k] == nums[k - 1]) k++;
+                        while (k < l && nums[l] == nums[l + 1]) l--;
                     }
                     else if (sum < target) {
-                        left++;
+                        k++;
                     }
                     else {
-                        right--;
+                        l--;
                     }
                 }
             }
         }
-        return res;
-        
+        return ans;
     }
 }
