71f0d4c1d0dbed14000c4af73cf002d1ed1b0044
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-03-16 18:19:01 +0530

    Time: 130 ms (19.31%), Space: 46.8 MB (83.42%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.java
+++ 0454-4sum-ii/0454-4sum-ii.java
@@ -1,28 +1,30 @@
-import java.util.*;
-
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
 
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        // store frequency of nums4
-        for (int num : nums4) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        // store sums of nums1 and nums2
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+
+                int sum = nums1[i] + nums2[j];
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+
+            }
         }
 
         int count = 0;
 
-        for (int i = 0; i < nums1.length; i++) {
-            for (int j = 0; j < nums2.length; j++) {
-                for (int k = 0; k < nums3.length; k++) {
+        // check nums3 and nums4
+        for (int i = 0; i < nums3.length; i++) {
+            for (int j = 0; j < nums4.length; j++) {
 
-                    int sum = nums1[i] + nums2[j] + nums3[k];
+                int sum = nums3[i] + nums4[j];
 
-                    if (map.containsKey(-sum)) {
-                        count += map.get(-sum);
-                    }
-
+                if (map.containsKey(-sum)) {
+                    count += map.get(-sum);
                 }
+
             }
         }
 
