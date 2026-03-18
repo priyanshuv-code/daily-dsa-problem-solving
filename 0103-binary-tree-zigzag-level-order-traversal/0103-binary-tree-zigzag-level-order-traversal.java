f438c19fab32f7cfccf55068a8521100d7e17842
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-03-18 08:51:17 +0530

    Time: 1 ms (77.84%), Space: 43.4 MB (94.15%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -1,37 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        List<List<Integer>> ans = new LinkedList<>();
-
-        if (root == null) return ans;
-
+        List<List<Integer>> ans=new ArrayList<>();
+        if(root==null)return ans;
+        Queue<TreeNode> queue =new LinkedList<>();
+        boolean LefttoRight=true;
         queue.offer(root);
-        int level = 0;   // FIX: track level
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            ArrayList<Integer> list=new ArrayList<>();
+            for(int i=0;i<size;i++){
+                TreeNode node=queue.poll();
 
-        while (!queue.isEmpty()) {   // FIX 1
-            int size = queue.size();
-            List<Integer> list = new LinkedList<>();
-
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-
-                // FIX 2: zigzag based on level (not i)
-                if (level % 2 == 0) {
+                if(LefttoRight){
                     list.add(node.val);
-                } else {
-                    list.add(0, node.val);
                 }
-
-                // FIX 3: children always added normally
-                if (node.left != null) queue.offer(node.left);
-                if (node.right != null) queue.offer(node.right);
+                else{
+                    list.add(0,node.val);
+                }
+                if(node.left!=null)queue.offer(node.left);
+                if(node.right!=null)queue.offer(node.right);
             }
-
             ans.add(list);
-            level++;   // FIX 4: move to next level
+            LefttoRight=!LefttoRight;
         }
-
-        return ans;
+       return ans; 
     }
 }
