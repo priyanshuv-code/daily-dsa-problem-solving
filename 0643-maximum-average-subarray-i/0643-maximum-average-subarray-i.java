3df8207781f530584a867a1c69813dc5ae100429
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-03-19 10:54:17 +0530

    Time: 5 ms (39.33%), Space: 69 MB (97.58%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -3,17 +3,21 @@ class Solution {
         double max=Integer.MIN_VALUE;
         int i=0;
         int j=0;
-        int n=nums.length;
         int sum=0;
         while(j<nums.length){
             sum+=nums[j];
-            if(j-i+1==k){
+
+            if(j-i+1<k){
+                j++;
+            }
+            else if(j-i+1==k){
                 max=Math.max(max,sum);
                 sum-=nums[i];
                 i++;
+                j++;
             }
-            j++;
         }
         return max/k;
+        
     }
 }
