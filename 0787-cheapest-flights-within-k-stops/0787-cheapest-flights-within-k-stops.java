f27a75746237b84837cf11d532e63fad3d354959
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-04-25 13:09:00 +0530

    Time: 6 ms (86.05%), Space: 46.7 MB (38.82%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -0,0 +1,46 @@
+class Solution {
+
+    class Node {
+        int city, cost, stops;
+        Node(int city, int cost, int stops) {
+            this.city = city;
+            this.cost = cost;
+            this.stops = stops;
+        }
+    }
+
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+
+        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
+
+        for (int[] f : flights) {
+            adj.get(f[0]).add(new int[]{f[1], f[2]});
+        }
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(src, 0, 0));
+
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        while (!q.isEmpty()) {
+            Node cur = q.poll();
+
+            if (cur.stops > k) continue;
+
+            for (int[] it : adj.get(cur.city)) {
+                int next = it[0];
+                int price = it[1];
+
+                if (cur.cost + price < dist[next]) {
+                    dist[next] = cur.cost + price;
+                    q.add(new Node(next, dist[next], cur.stops + 1));
+                }
+            }
+        }
+
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+    }
+}
