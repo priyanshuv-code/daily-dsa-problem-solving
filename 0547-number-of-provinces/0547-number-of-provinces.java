183fae447cda1dd8bfbbbc3d644c582ec0a2f685
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-04-28 00:15:32 +0530

    Time: 3 ms (27.31%), Space: 49.9 MB (5.69%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,30 +1,40 @@
 class Solution {
-    public int findCircleNum(int[][] adj) {
-        int n=adj.length;
-        boolean []vis=new boolean [n];
-        int count=0;
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                bfs(i,adj,vis);
-                count++;
+    public void bfs(int node,Queue<Integer> q,ArrayList<ArrayList<Integer>> adj,boolean [] vis){
+        vis[node]=true;
+        q.add(node);
+        while(!q.isEmpty()){
+            int curr=q.poll();
+
+            for(int it:adj.get(curr)){
+                if(!vis[it]){
+                    vis[it]=true;
+                    q.add(it);
                 }
             }
-            return count; 
         }
-       
-        private void bfs(int i,int [][] adj,boolean [] vis){
-            Queue<Integer> q=new LinkedList<>();
-            q.add(i);
-            vis[i]=true;
-            while(!q.isEmpty()){
-                int node=q.poll();
-                for(int j=0;j<adj.length;j++){
-                    if(adj[node][j]==1 && vis[j]==false){
-                        vis[j]=true;
-                        q.add(j);
-                    }
-                }
-            } 
-        }
-        
     }
+    public int findCircleNum(int[][] isConnected) {
+        int n=isConnected.length;
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1 && i!=j){
+                    adj.get(i).add(j);
+                }
+            }
+        }
+        int cnt=0;
+        Queue<Integer> q=new LinkedList<>();
+        boolean [] vis=new boolean[n];
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                bfs(i,q,adj,vis);
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
