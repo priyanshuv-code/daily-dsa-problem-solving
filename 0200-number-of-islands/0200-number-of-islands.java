97e5febe8df97685638605ceb7f270519fa9266a
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-04-30 14:55:44 +0530

    Time: 6 ms (19.85%), Space: 50.6 MB (99.84%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -7,44 +7,43 @@ class Solution {
             this.col=col;
         }
     }
-    public void bfs(int start,int row,int [][]vis,char[][] grid){
-        int n = grid.length;
-        int m = grid[0].length;
-
+    public void bfs(int row,int col,int [][] vis,char [][]grid){
+        int n=grid.length;
+        int m=grid[0].length;
         Queue<pair> q=new LinkedList<>();
-        q.add(new pair(start,row));
-        vis[start][row]=1;
-        int []dr={-1,0,0,1};
-        int []dc={0,-1,1,0};
+        q.add(new pair(row,col));
+        vis[row][col]=1;
+        int []dr={-1,0,1,0};
+        int []dc={0,1,0,-1};
         while(!q.isEmpty()){
             pair curr=q.poll();
 
             for(int i=0;i<4;i++){
-                int rowr=curr.row+dr[i];
-                int colc=curr.col+dc[i];
+                int newr=curr.row+dr[i];
+                int newc=curr.col+dc[i];
 
-                if(rowr>=0 && rowr<n &&  colc>=0 && colc<m &&  grid[rowr][colc]=='1' && vis[rowr][colc]==0){
-                    q.add(new pair(rowr,colc));
-                    vis[rowr][colc]=1;
+                if(newr>=0 && newr<n && newc>=0 && newc<m && vis[newr][newc]==0 && grid[newr][newc]=='1'){
+                    vis[newr][newc]=1;
+                    q.add(new pair(newr,newc));
                 }
             }
         }
     }
     public int numIslands(char[][] grid) {
-        int count=0;
         int n=grid.length;
         int m=grid[0].length;
-        int [][]vis=new int[n][m];
 
-        
+        int [][]vis=new int[n][m];
+        int cnt=0;
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(vis[i][j]==0 && grid[i][j]=='1'){
                     bfs(i,j,vis,grid);
-                    count++;
+                    cnt++;
                 }
             }
         }
-        return count;
-    } 
+        return cnt;
+    }
 }
