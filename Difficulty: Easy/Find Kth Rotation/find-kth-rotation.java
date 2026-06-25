36ba6ae5a6a859dbae170842c88c33deeee04768
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-06-25 15:55:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find Kth Rotation/find-kth-rotation.java
+++ Difficulty: Easy/Find Kth Rotation/find-kth-rotation.java
@@ -1,20 +1,37 @@
 class Solution {
-    public int findKRotation(int nums[]) {
-        // Code here
-        int n=nums.length;
-        int left=0;
-        int right=n-1;
-        
-        while(left<right){
-            int mid=(left+right)/2;
-            
-            if(nums[mid]<nums[right]){
-                right=mid;
+    int findKRotation(int arr[]) {
+
+        int low = 0;
+        int high = arr.length - 1;
+        int ans = Integer.MAX_VALUE;
+        int index = -1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            // Left half is sorted
+            if (arr[low] <= arr[mid]) {
+
+                if (arr[low] < ans) {
+                    ans = arr[low];
+                    index = low;
+                }
+
+                low = mid + 1;
             }
-            else{
-                left=mid+1;
+            // Right half is sorted
+            else {
+
+                if (arr[mid] < ans) {
+                    ans = arr[mid];
+                    index = mid;
+                }
+
+                high = mid - 1;
             }
         }
-        return left;
+
+        return index;
     }
 }
