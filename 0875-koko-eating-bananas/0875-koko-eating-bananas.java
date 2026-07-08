9a6fda87a037e1735802b1a03a8e71615fa7d19a
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-07-08 15:21:44 +0530

    Time: 7 ms (83.01%), Space: 48.5 MB (10.76%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,24 +1,30 @@
 class Solution {
+    public boolean checkeating(int []piles,int k,int h){
+        long cnt=0;
+        for(int i=0;i<piles.length;i++){
+            cnt+=(piles[i]+k-1)/k;
+        }
+        if(cnt<=h){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
     public int minEatingSpeed(int[] piles, int h) {
         int n=piles.length;
-        int low=0;
+        int low=1;
         int high=0;
-        int ans=0;
-
         for(int i=0;i<n;i++){
+            low=Math.min(low,piles[i]);
             high=Math.max(high,piles[i]);
-            low+=piles[i];
         }
-        low=low/h;
-
+        int ans=0;
         while(low<=high){
             int mid=low+(high-low)/2;
+            boolean caneat=checkeating(piles,mid,h);
 
-            long cnt=0;
-            for(int i=0;i<n;i++){
-                cnt+=(int)Math.ceil((double) piles[i]/mid);
-            }
-            if(cnt<=h){
+            if(caneat==true){
                 ans=mid;
                 high=mid-1;
             }
