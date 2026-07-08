66a1bde49db22f121d9e4bd0e1523e4a597a49e7
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-07-09 03:00:24 +0530

    Time: 93 ms (63.43%), Space: 286.7 MB (33.99%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,34 +1,36 @@
 class Solution {
-    public boolean validPath(int n, int[][] edges, int start, int end) {
-        if(start==end) return true;
-        List<List<Integer>> adj=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            List<Integer> list=new ArrayList<>();
-            adj.add(list);
-        }
-
-        for(int i=0;i<edges.length;i++){
-            int a=edges[i][0], b=edges[i][1];
-            adj.get(a).add(b);
-            adj.get(b).add(a);
-        }
-        boolean [] vis=new boolean[n];
-        vis[start]=true;
-        bfs(start,adj,vis);
-        return vis[end];
-    }
-    private void bfs(int start,List<List<Integer>> adj,boolean [] vis){
+    public void bfs(int node,ArrayList<ArrayList<Integer>> adj,boolean [] vis){
         Queue<Integer> q=new LinkedList<>();
-        q.add(start);
-        while(q.size()>0){
-            int front=q.remove();
-            for(int ele:adj.get(front)){
-                if(!vis[ele]){
-                    q.add(ele);
-                    vis[ele]=true;
+        q.add(node);
+
+        while(!q.isEmpty()){
+            int curr=q.poll();
+
+            for(int adje:adj.get(curr)){
+                if(!vis[adje]){
+                    vis[adje]=true;
+                    q.add(adje);
                 }
             }
         }
     }
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
 
+        for(int i=0;i<edges.length;i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        boolean [] vis=new boolean[n];
+        vis[source]=true;
+        bfs(source,adj,vis);
+        return vis[destination];
+
+    }
 }
