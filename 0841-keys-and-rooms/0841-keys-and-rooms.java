b196a154d55c23dfe691a394ed1c1edea7f831af
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-07-08 21:48:58 +0530

    Time: 6 ms (5.31%), Space: 46.4 MB (45.49%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,25 +1,37 @@
 class Solution {
-    public boolean canVisitAllRooms(List<List<Integer>> adj) {
-        int n=adj.size();
-        boolean [] visited=new boolean[n];
-        visited[0]=true;
-        bfs(0,adj,visited);
-        for(boolean ele:visited){
-            if(ele==false)return false;;
-        }
-        return true;
-    }
-    private void bfs(int start,List<List<Integer>> adj,boolean []visited){
-        Queue<Integer> q=new LinkedList<>();
-        q.add(start);
-        while(q.size()>0){
-            int front=q.remove();
-            for(int ele:adj.get(front)){
-                if(!visited[ele]){
-                    visited[ele]=true;
-                    q.add(ele);
+    public void bfs(int node,ArrayList<ArrayList<Integer>> adj,boolean [] vis,Queue<Integer> q){ 
+        q.add(node);
+        vis[node]=true;
+        
+        while(!q.isEmpty()){
+            int curr=q.poll();
+
+            for(int adje:adj.get(curr)){
+                if(!vis[adje]){
+                    vis[adje]=true;
+                    q.add(adje);
                 }
             }
         }
+    }
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        int n=rooms.size();
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<n;i++){
+            for(int it:rooms.get(i)){
+                adj.get(i).add(it);
+            }
+        }
+        boolean[] vis=new boolean[n];
+        Queue<Integer> q=new LinkedList<>();
+        bfs(0,adj,vis,q);
+        for(int i=0;i<n;i++){
+            if(vis[i]==false)return false;
+        }
+        return true;
     }
 }
