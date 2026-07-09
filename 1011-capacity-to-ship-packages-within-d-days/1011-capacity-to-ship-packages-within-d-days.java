a86c079bcd798b94173dae11e7d90b22706244ef
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-07-09 21:38:05 +0530

    Time: 12 ms (28.39%), Space: 50.3 MB (19.79%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,28 +1,31 @@
 class Solution {
+    public boolean ispossible(int [] weights,int mid,int days){
+        int cnt=1;
+        int sum=0;
+        for(int i=0;i<weights.length;i++){
+            if(sum+weights[i]>mid){
+                cnt++;
+                sum=weights[i];
+            }
+            else{
+                sum+=weights[i];
+            }
+        }
+        return cnt<=days;
+    }
     public int shipWithinDays(int[] weights, int days) {
         int n=weights.length;
         int low=0;
         int high=0;
-        int ans=0;
-
         for(int i=0;i<n;i++){
-            low=Math.max(low,weights[i]);
+            low = Math.max(low, weights[i]);
             high+=weights[i];
         }
+        int ans=0;
         while(low<=high){
             int mid=low+(high-low)/2;
 
-            int cnt=1;
-            int sum=weights[0];
-            for(int i=1;i<n;i++){
-                sum+=weights[i];
-
-                if(sum>mid){
-                    sum=weights[i];
-                    cnt++;
-                }
-            }
-            if(cnt<=days){
+            if(ispossible(weights,mid,days)){
                 ans=mid;
                 high=mid-1;
             }
