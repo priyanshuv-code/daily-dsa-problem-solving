e5feb7350d151a88f9e3d95781c3201e609558ee
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-07-14 16:50:39 +0530

    Time: 18 ms (25.52%), Space: 44.1 MB (26.57%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,24 +1,32 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character,Character> map1=new HashMap<>();
-        HashMap<Character,Character> map2=new HashMap<>();
 
-        for(int i=0;i<s.length();i++){
-            char ch1=s.charAt(i);
-            char ch2=t.charAt(i);     
-            if(map1.containsKey(ch1) && map2.containsKey(ch2)){
-                if(map1.get(ch1)!=ch2 || map2.get(ch2)!=ch1){
+        if (s.length() != t.length())
+            return false;
+
+        HashMap<Character, Character> map1 = new HashMap<>();
+        HashMap<Character, Character> map2 = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            if (map1.containsKey(c1)) {
+                if (map1.get(c1) != c2)
                     return false;
-                }
+            } else {
+                map1.put(c1, c2);
             }
-            else if(!map1.containsKey(ch1) && !map2.containsKey(ch2) ){
-                map1.put(ch1,ch2);
-                map2.put(ch2,ch1);
-            }
-            else{
-                return false;
+
+            if (map2.containsKey(c2)) {
+                if (map2.get(c2) != c1)
+                    return false;
+            } else {
+                map2.put(c2, c1);
             }
         }
+
         return true;
     }
 }
