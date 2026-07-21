ae5095f910c18bef4623eebbed547ee6573e4706
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-07-21 13:45:58 +0530

    Time: 146 ms (7.52%), Space: 46.5 MB (98.61%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.java
+++ 0454-4sum-ii/0454-4sum-ii.java
@@ -1,33 +1,26 @@
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        // store sums of nums1 and nums2
-        for (int i = 0; i < nums1.length; i++) {
-            for (int j = 0; j < nums2.length; j++) {
-
-                int sum = nums1[i] + nums2[j];
-                map.put(sum, map.getOrDefault(sum, 0) + 1);
-
-            }
-        }
-
-        int count = 0;
-
-        // check nums3 and nums4
-        for (int i = 0; i < nums3.length; i++) {
-            for (int j = 0; j < nums4.length; j++) {
-
-                int sum = nums3[i] + nums4[j];
-
-                if (map.containsKey(-sum)) {
-                    count += map.get(-sum);
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums1.length;i++){
+            for(int j=0;j<nums2.length;j++){
+                int sum=nums1[i]+nums2[j];
+                if(map.containsKey(sum)){
+                    map.put(sum,map.get(sum)+1); 
                 }
-
+                else{
+                    map.put(sum,1);
+                }
             }
         }
-
-        return count;
+        int cnt=0;
+        for(int i=0;i<nums3.length;i++){
+            for(int j=0;j<nums4.length;j++){
+                int sum=nums3[i]+nums4[j];
+                if(map.containsKey(-sum)){
+                    cnt+=map.get(-sum);
+                }
+            }
+        }
+        return cnt;
     }
 }
