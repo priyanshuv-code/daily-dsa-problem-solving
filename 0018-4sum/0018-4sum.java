026c74763e32c4c606606a42e22cfe8b4820e5af
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-07-21 12:50:20 +0530

    Time: 69 ms (10.55%), Space: 47.4 MB (7.95%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,31 +1,33 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-
         Arrays.sort(nums);
         Set<List<Integer>> set=new HashSet<>();
         int n=nums.length;
-        for(int i=0;i<n-3;i++){
-            for(int j=i+1;j<n-2;j++){
-                int left=j+1;
-                int right=n-1;
-
-                while(left<right){
-                    long sum=((long)nums[i]+nums[j]+nums[left]+nums[right]);
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n-1;j++){
+                int k=j+1;
+                int l=n-1;
+                while(k<l){
+                    long sum=(long)nums[i]+nums[j]+nums[k]+nums[l];
                     if(sum==target){
-                        set.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
-                        left++;
-                        right--;;
+                        List<Integer> ls=new ArrayList<>();
+                        ls.add(nums[i]);
+                        ls.add(nums[j]);
+                        ls.add(nums[k]);
+                        ls.add(nums[l]);
+                        set.add(ls);
+                        k++;
+                        l--;
                     }
-                    else if(sum<target){
-                        left++;
+                    else if(sum>target){
+                        l--;
                     }
                     else{
-                        right--;
+                        k++;
                     }
                 }
             }
         }
-        
         return new ArrayList<>(set);
     }
 }
