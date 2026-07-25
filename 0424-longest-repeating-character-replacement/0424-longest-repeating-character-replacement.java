c0f1d414aa227881cd56b8c3d7fecdd98be90373
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-07-25 14:35:44 +0530

    Time: 31 ms (17.68%), Space: 46.9 MB (26.48%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -3,24 +3,22 @@ class Solution {
         int n=s.length();
         int i=0;
         int j=0;
-        HashMap<Character,Integer> map=new HashMap<>();
         int ans=0;
         int max_freq=0;
+        HashMap<Character,Integer> map=new HashMap<>();
         while(j<n){
-            char ch=s.charAt(j);
-            if(map.containsKey(ch)){
-                map.put(ch,map.get(ch)+1);
+            char jth=s.charAt(j);
+            if(map.containsKey(jth)){
+                map.put(jth,map.get(jth)+1);
             }
             else{
-                map.put(ch,1);
+                map.put(jth,1);
             }
-            max_freq=Math.max(max_freq,map.get(ch));
-            while(j-i+1-max_freq>k){
+            max_freq=Math.max(max_freq,map.get(jth));
+            while((j-i+1-max_freq)>k){
                 char ith=s.charAt(i);
                 map.put(ith,map.get(ith)-1);
-                if(map.get(ith)==0){
-                    map.remove(ith);
-                }
+                if(map.get(ith)==0)map.remove(ith);
                 i++;
             }
             ans=Math.max(ans,j-i+1);
@@ -29,11 +27,3 @@ class Solution {
         return ans;
     }
 }
-
-
-
-
-
-
-
-
