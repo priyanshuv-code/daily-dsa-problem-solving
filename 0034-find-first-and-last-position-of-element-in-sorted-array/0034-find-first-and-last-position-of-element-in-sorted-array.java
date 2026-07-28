d80e4ddd850bf4ab6696a9e7c5bd88afa9f586b7
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-07-28 14:25:02 +0530

    Time: 0 ms (100%), Space: 48.1 MB (71.86%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,52 +1,44 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int n=nums.length;
-        if(n==1){
-            if(nums[0]==target) return new int[]{0,0};
-        }
         int []ans=new int[2];
-        ans[0]=seachfirst(nums,target);
-        ans[1]=seachlast(nums,target);
+        ans[0]=binary_fir(nums,target);
+        ans[1]=binary_last(nums,target);
         return ans;
     }
-    public int seachfirst(int []nums,int target){
-        int n=nums.length;
-        int low=0;
-        int high=n-1;
+    public static int binary_fir(int []nums,int target){
+        int i=0;
+        int j=nums.length-1;
         int ans=-1;
-
-        while(low<=high){
-            int mid=low+(high-low)/2;
+        while(i<=j){
+            int mid=i+(j-i)/2;
             if(nums[mid]==target){
-                ans=mid;
-                high=mid-1;
+                ans=mid;;
+                j=mid-1;;
             }
-            else if(nums[mid]<target){
-                low=mid+1;
+            else if(nums[mid]>target){
+                j=mid-1;
             }
             else{
-                high=mid-1;
+                i=mid+1;
             }
         }
         return ans;
-    } 
-    public int seachlast(int []nums,int target){
-        int n=nums.length;
-        int low=0;
-        int high=n-1;
+    }
+    public static int binary_last(int []nums,int target){
+        int i=0;
+        int j=nums.length-1;
         int ans=-1;
-
-        while(low<=high){
-            int mid=low+(high-low)/2;
+        while(i<=j){
+            int mid=i+(j-i)/2;
             if(nums[mid]==target){
                 ans=mid;
-                low=mid+1;
+                i=mid+1;
             }
-            else if(nums[mid]<target){
-                low=mid+1;
+            else if(nums[mid]>target){
+                j=mid-1;
             }
             else{
-                high=mid-1;
+                i=mid+1;
             }
         }
         return ans;
