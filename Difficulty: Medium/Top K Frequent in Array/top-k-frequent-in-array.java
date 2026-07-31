47a9bf1ce9588e06b9553d44989e78228e79694a
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-07-31 20:42:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Top K Frequent in Array/top-k-frequent-in-array.java
@@ -0,0 +1,37 @@
+class Pair implements Comparable<Pair>{
+    int ele;
+    int freq;
+    Pair(int ele,int freq){
+        this.ele=ele;
+        this.freq=freq;
+    }
+    public int compareTo(Pair p){
+        if(this.freq==p.freq){
+            return this.ele-p.ele;
+        }
+        return this.freq-p.freq;
+    }
+}
+class Solution {
+    public ArrayList<Integer> topKFreq(int[] arr, int k) {
+        // Code here
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int ele:arr){
+            map.put(ele,map.getOrDefault(ele,0)+1);
+        }
+        PriorityQueue<Pair> pq=new PriorityQueue<>();
+        
+        for(int key:map.keySet()){
+            int freq=map.get(key);
+            pq.add(new Pair(key,freq));
+            if(pq.size()>k)pq.remove();
+        }
+        ArrayList<Integer> ans=new ArrayList<>();
+        while(pq.size()>0){
+            Pair top=pq.remove();
+            ans.add(top.ele);
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+}
