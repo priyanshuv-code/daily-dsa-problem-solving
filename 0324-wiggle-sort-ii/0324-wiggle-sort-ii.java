9c70abaab2fe9e209111d4c5b4564cde649d7666
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-08-06 14:27:08 +0530

    Time: 9 ms (59.21%), Space: 48.8 MB (59.45%) - LeetHub

---
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
@@ -0,0 +1,38 @@
+class Solution {
+    public void reverse(int start,int end,int []arr){
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+    public void wiggleSort(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        int[] arr1 = new int[(n + 1) / 2];
+        int[] arr2 = new int[n / 2];
+        int idx=0;
+        for(int i=0;i<arr1.length;i++){
+            arr1[idx++]=nums[i];
+        }
+        int k=0;
+        for(int i=arr1.length;i<n;i++){
+            arr2[k++]=nums[i];
+        }
+        reverse(0,arr1.length-1,arr1);
+        reverse(0,arr2.length-1,arr2);
+        int s=0;
+        int l=0;
+        for(int i=0;i<n;i++){
+            if(i%2!=0){
+                nums[i]=arr2[l++];
+            }
+            else{
+                nums[i]=arr1[s++];
+            }
+        }
+        
+    }
+}
