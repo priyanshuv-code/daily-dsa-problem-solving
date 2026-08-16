d572522f4d54ff9de7d4d62eb00393132065680f
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-08-16 22:07:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Anagram/anagram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static boolean areAnagrams(String s1, String s2) {
+        // code here
+        char []ch1=s1.toCharArray();
+        char []ch2=s2.toCharArray();
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        if(ch1.length!=ch2.length)return false;
+        for(int i=0;i<ch1.length;i++){
+            if(ch1[i]!=ch2[i])return false;
+        }
+        return true;
+    }
+}
