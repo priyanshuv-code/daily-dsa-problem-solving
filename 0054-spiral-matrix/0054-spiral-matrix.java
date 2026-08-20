8d290b18650f511159c513b43c3431f5f121fc7a
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-08-20 07:23:18 +0530

    Time: 0 ms (100%), Space: 43.4 MB (9.77%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,35 +1,35 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-       ArrayList<Integer> ans=new ArrayList<>();
-       int m=matrix.length;
-       int n=matrix[0].length;
-       int top=0;
-       int left=0;
-       int bottom=m-1;
-       int right=n-1;
-       int total=0;
-       while(total<m*n){
-        for(int i=left;i<=right && total<m*n;i++){
-            ans.add(matrix[top][i]);
-            total++;
+        ArrayList<Integer> ans=new ArrayList<>();
+        int m=matrix.length; // rows
+        int n=matrix[0].length; // cols
+        int top=0;
+        int bottom=m-1;
+        int left=0;
+        int right=n-1;
+        int total=0;
+        while(total<m*n){
+            for(int i=left;i<=right && total<m*n;i++){
+                ans.add(matrix[top][i]);
+                total++;
+            }
+            top++;
+            for(int i=top;i<=bottom && total<m*n;i++){
+                ans.add(matrix[i][right]);
+                total++;
+            }
+            right--;
+            for(int i=right;i>=left && total<m*n;i--){
+                ans.add(matrix[bottom][i]);
+                total++;
+            }
+            bottom--;
+            for(int i=bottom;i>=top && total<m*n;i--){
+                ans.add(matrix[i][left]);
+                total++;
+            }
+            left++;
         }
-        top++;
-        for(int i=top;i<=bottom && total<m*n;i++){
-            ans.add(matrix[i][right]);
-            total++;
-        }
-        right--;
-        for(int i=right;i>=left && total<m*n;i--){
-            ans.add(matrix[bottom][i]);
-            total++;
-        }
-        bottom--;
-        for(int i=bottom;i>=top && total<m*n;i--){
-            ans.add(matrix[i][left]);
-            total++;
-        }
-        left++;
-       }
-       return ans;
+        return ans;
     }
 }
