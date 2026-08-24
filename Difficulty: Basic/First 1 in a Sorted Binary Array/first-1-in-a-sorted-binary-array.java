6198cede611d67b7e61cc24965c56b8f6d30a017
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-08-24 11:58:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/First 1 in a Sorted Binary Array/first-1-in-a-sorted-binary-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int firstIndex(int arr[]) {
+        // code here
+        int n=arr.length;
+        int start=0;
+        int end=n-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==1)end=mid-1;
+            else{
+            
+                start=mid+1;
+            }
+        }
+        if(start==n)return -1;
+        return start;
+    }
+}
