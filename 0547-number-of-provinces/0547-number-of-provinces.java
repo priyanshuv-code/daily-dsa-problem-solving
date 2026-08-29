0ef01f517f5ea55645008b120ae30454cef3ae36
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-08-29 18:56:25 +0530

    Time: 5 ms (8.33%), Space: 49.6 MB (10.51%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,39 +1,37 @@
 class Solution {
-    public void bfs(int node ,ArrayList<ArrayList<Integer>> adj,Queue<Integer> q,boolean []vis){
-        q.add(node);
-        vis[node]=true;
-
+    public void bfs(int start,ArrayList<ArrayList<Integer>> list,Queue<Integer> q,boolean [] vis){
+        q.add(start);
+        vis[start]=true;
         while(!q.isEmpty()){
-            int curr=q.poll();
-
-            for(int adje:adj.get(curr)){
-                if(!vis[adje]){
-                    vis[adje]=true;
-                    q.add(adje);
+            int node=q.poll();
+            for(int adj:list.get(node)){
+                if(!vis[adj]){
+                    q.add(adj);
+                    vis[adj]=true;
                 }
             }
         }
     }
     public int findCircleNum(int[][] isConnected) {
         int n=isConnected.length;
-
-        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        
+        ArrayList<ArrayList<Integer>> list=new ArrayList<>();
         for(int i=0;i<n;i++){
-            adj.add(new ArrayList<>());
+            list.add(new ArrayList<>());
         }
+        Queue<Integer> q=new LinkedList<>();
+        boolean []vis=new boolean[n];
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1 && i!=j){
-                    adj.get(i).add(j);
+                if(isConnected[i][j]==1){
+                list.get(i).add(j);
                 }
             }
         }
         int cnt=0;
-        Queue<Integer> q=new LinkedList<>();
-        boolean []vis=new boolean[n];
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                bfs(i,adj,q,vis);
+                bfs(i,list,q,vis);
                 cnt++;
             }
         }
