911467a41dbfc69970cd08126ad6660707acc1e6
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-08-30 16:47:00 +0530

    Time: 1 ms (39.2%), Space: 46.5 MB (90.36%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,65 +1,25 @@
 class Solution {
-    class pair{
-        int first;
-        int second;
-        pair(int first,int second){
-            this.first=first;
-            this.second=second;
-        }
-    }
-    public void bfs(int[][] grid,int sr,int sc,int color,boolean [][]vis){
-        int m=grid.length;
-        int n=grid[0].length;
-        Queue<pair> q=new LinkedList<>();
-        q.add(new pair(sr,sc));
-        vis[sr][sc] = true;
-        int ori=grid[sr][sc];
-        grid[sr][sc]=color;
-        while(!q.isEmpty()){
-            pair curr=q.poll();
-            int r=curr.first;
-            int c=curr.second;
-
-            // up -> row-1,col
-            if(r>0){
-                if(!vis[r-1][c] && grid[r-1][c]==ori){
-                    q.add(new pair(r-1,c));
-                    vis[r-1][c]=true;
-                    grid[r-1][c]=color;
-                }
-            }
-            // bottom -> row+1,col
-            if((r+1)<m){
-                if(!vis[r+1][c] && grid[r+1][c]==ori){
-                    q.add(new pair(r+1,c));
-                    vis[r+1][c]=true;
-                    grid[r+1][c]=color;
-                }
-            }
-            // left -> row,col-1
-            if(c>0){
-                if(!vis[r][c-1] && grid[r][c-1]==ori){
-                    q.add(new pair(r,c-1));
-                    vis[r][c-1]=true;
-                    grid[r][c-1]=color;
-                }
-            }
-            // right -> row,col+1
-            if((c+1)<n){
-                if(!vis[r][c+1] && grid[r][c+1]==ori){
-                    q.add(new pair(r,c+1));
-                    vis[r][c+1]=true;
-                    grid[r][c+1]=color;
-                }
-            }
-        }
-
-    }
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+    public void dfs(int r,int c,int[][] image,boolean [][]vis,int orginal,int color){
         int m=image.length;
         int n=image[0].length;
+         int []row={-1,0,1,0};
+         int []col={0,1,0,-1};
+         vis[r][c]=true;
+         image[r][c]=color;
+         for(int i=0;i<4;i++){
+            int newr=r+row[i];
+            int newc=c+col[i];
+            if(newr>=0  &&newr<m && newc>=0 && newc<n && !vis[newr][newc] && image[newr][newc]==orginal){
+                dfs(newr,newc,image,vis,orginal,color);
+            }
+         }
+    }
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int m=image.length; // no of rows;
+        int n=image[0].length; // no of cols;
+        int orginal = image[sr][sc];
         boolean [][]vis=new boolean[m][n];
-        bfs(image,sr,sc,color,vis);
-        return image;
+        dfs(sr,sc,image,vis,orginal,color);
+        return image;   
     }
 }
