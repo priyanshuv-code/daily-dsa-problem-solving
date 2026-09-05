f0838e65c28609f24208c8a7417481e132952b18
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-09-06 00:12:50 +0530

    Time: 3 ms (29.52%), Space: 47.7 MB (74.61%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,66 +1,68 @@
-class pair{
-    int row;
-    int col;
-    pair(int row,int col){
-        this.row=row;
-        this.col=col;
-    }
-}
 class Solution {
-    public void bfs(char [][] board,boolean [][]vis,int rows,int cols){
-        int m=board.length; // row; 
-        int n=board[0].length; //col
+    class pair{
+        int row;
+        int col;
+        pair(int row,int col){
+            this.row=row;
+            this.col=col;
+        }
+    }
+    public void bfs(int i,int j,char[][] board,int [][]vis){
+        int n=board.length; // no of rows..
+        int m=board[0].length; // no of cols..
         Queue<pair> q=new LinkedList<>();
-        q.add(new pair(rows,cols));
-        vis[rows][cols]=true;
-        int [] delrow={-1,0,1,0};
-        int [] delcol={0,1,0,-1};
+        vis[i][j]=1;
+        q.add(new pair(i,j));
+        int [] delrow={0,-1,0,1};
+        int [] delcol={1,0,-1,0};
         while(!q.isEmpty()){
-            pair top=q.poll();
-            int r=top.row;
-            int c=top.col;
-            for(int i=0;i<4;i++){
-                int nr=delrow[i]+r;
-                int nc=delcol[i]+c;
-                if(nr>=0 && nr<m && nc>=0 && nc<n && vis[nr][nc]==false && board[nr][nc]=='O'){
-                    vis[nr][nc]=true;
-                    q.add(new pair(nr,nc));
+            pair curr=q.poll();
+            int r=curr.row;
+            int c=curr.col;
+            for(int k=0;k<4;k++){
+                int newrow=r+delrow[k];
+                int newcol=c+delcol[k];
+               if(newrow >= 0 && newrow < n &&
+                    newcol >= 0 && newcol < m &&
+                    vis[newrow][newcol] == 0 &&
+                    board[newrow][newcol] == 'O'){
+                    vis[newrow][newcol]=1;
+                    q.add(new pair(newrow,newcol));
                 }
             }
         }
     }
     public void solve(char[][] board) {
-        int m=board.length;
-        int n=board[0].length;
-        boolean [][]vis=new boolean[m][n];
+        int n=board.length; // no of rows..
+        int m=board[0].length; // no of cols..
+
+        int [][]vis=new int[n][m];
+        for(int i=0;i<m;i++){
+            if(vis[0][i]==0 && board[0][i]=='O'){
+                bfs(0,i,board,vis);
+            }
+        }
+        for(int i=0;i<m;i++){
+            if(vis[n-1][i]==0 && board[n-1][i]=='O'){
+                bfs(n-1,i,board,vis);
+            }
+        }
         for(int i=0;i<n;i++){
-            if(!vis[0][i] && board[0][i]=='O'){
-                bfs(board, vis, 0, i);
+            if(vis[i][0]==0 && board[i][0]=='O'){
+                bfs(i,0,board,vis);
             }
         }
         for(int i=0;i<n;i++){
-            if(!vis[m-1][i] && board[m-1][i]=='O'){
-                bfs(board, vis, m-1, i);
+            if(vis[i][m-1]==0 && board[i][m-1]=='O'){
+                bfs(i,m-1,board,vis);
             }
         }
-        for(int i=0;i<m;i++){
-            if(!vis[i][0] && board[i][0]=='O'){
-                bfs(board, vis, i, 0);
-            }
-        }
-        for(int i=0;i<m;i++){
-            if(!vis[i][n-1] && board[i][n-1]=='O'){
-                bfs(board, vis, i, n-1);
-            }
-        }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(!vis[i][j] && board[i][j]=='O'){
-                    vis[i][j]=true;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]==0 && board[i][j]=='O'){
                     board[i][j]='X';
                 }
             }
         }
-
-    } 
+    }
 }
