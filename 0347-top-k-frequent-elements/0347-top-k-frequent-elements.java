0b9715e6080e0cc0652322239f7ef72aa83cb3fa
Priyanshu Verma <pkverma012345678910@gmail.com>
2026-09-15 15:14:25 +0530

    Time: 60 ms (5.02%), Space: 48.6 MB (8.86%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,35 +1,22 @@
-class Pair implements Comparable<Pair>{
-    int ele;
-    int freq;
-    Pair(int ele,int freq){
-        this.ele=ele;
-        this.freq=freq;
-    }
-    public int compareTo(Pair p){
-        if(this.freq==p.freq){
-            return this.ele-p.ele;
-        }
-        return this.freq-p.freq;
-    }
-}
 class Solution {
-    public int[] topKFrequent(int[] arr, int k) {
-         HashMap<Integer,Integer> map=new HashMap<>();
-        for(int ele:arr){
-            map.put(ele,map.getOrDefault(ele,0)+1);
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        PriorityQueue<Pair> pq=new PriorityQueue<>();
-        
-        for(int key:map.keySet()){
-            int freq=map.get(key);
-            pq.add(new Pair(key,freq));
-            if(pq.size()>k)pq.remove();
-        }
-        int []ans=new int[pq.size()];
-        int idx=pq.size()-1;
-        while(pq.size()>0){
-            Pair top=pq.remove();
-            ans[idx--]=top.ele;
+        int []ans=new int[k];
+        for(int i=0;i<k;i++){
+            int max_f=-1;
+            int max_val=-1;
+            for(int f:map.keySet()){
+                if(map.get(f)>max_val){
+                    max_f=f;
+                    max_val=map.get(f);
+                }
+            }
+            ans[i]=max_f;
+            map.remove(max_f);
+
         }
         return ans;
     }
